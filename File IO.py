cd70d88ab29cf1005cf51a1736ce86a8ecf75f59
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2020-02-19 18:42:00 +0530

    Delete File IO.py

--- File IO.py
+++
@@ -1,8 +0,0 @@
-import os
-print(os.listdir())
-print(os.getcwd())
-
-with open('/home/rohan/FacialRecognition/helloworld', 'a') as file:
-    for i in range(10):
-        file.write('HelloPython')
-    file.close()

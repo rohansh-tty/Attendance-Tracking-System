a598e0a5e56f004d00eabd8b186e5a027d5b098f
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2020-03-02 08:59:42 +0530

    CodeUpdated

--- face_recognition-fasterOne.py
+++ face_recognition-fasterOne.py
@@ -14,6 +14,7 @@ for filename in os.listdir('Dataset'):
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
+# Names: Barack Obama, Joe Biden
 obama_image = face_recognition.load_image_file("obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 

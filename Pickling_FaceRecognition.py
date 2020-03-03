f5d184751d2dd752be49d2b72e737feaa5187272
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2020-03-03 13:17:58 +0530

    CodeUpdated

--- Pickling_FaceRecognition.py
+++ Pickling_FaceRecognition.py
@@ -22,8 +22,8 @@ for filename in os.listdir('NewTest'):
     all_face_encodings[filename.split('.')[0]] = face_recognition.face_encodings(im)[0]
     images.append(im)
     rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-# detect the (x, y)-coordinates of the bounding boxes
-# corresponding to each face in the input image
+
+    # detect the (x, y)-coordinates of the bounding boxes
     boxes = face_recognition.face_locations(rgb)
 # compute the facial embedding for the face
     encodings = face_recognition.face_encodings(rgb, boxes)
